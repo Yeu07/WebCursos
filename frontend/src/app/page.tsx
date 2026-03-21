da431770baf5bd54d6c24ed1133ecb15da96c1f3
Yeu <yeumensilva9@gmail.com>
2026-03-21 20:41:21 -0300

    Push to home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,22 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+
+    router.push("/Cursos")
+
+    return () => {
+      
+    };
+  }, []);
+
   return (
-    <h1>Hola, esto es Next.js</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
