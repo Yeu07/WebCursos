a99fd0aca9d000063219beb413de4e4c241eb461
Yeu <yeumensilva9@gmail.com>
2026-03-21 17:20:02 -0300

    Install core types ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express, { type Response } from 'express';
+import cors from 'cors';
 import courseRouter from './routes/courseRoutes.js';
 
 const app = express();
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors())
 app.use("/cursos",courseRouter)
 
 
