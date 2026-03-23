5a8edea00891afbfaea126aed1a6235c5994e97d
Yeu <yeumensilva9@gmail.com>
2026-03-22 21:06:30 -0300

    Separate server and client

--- frontend/src/app/Inicio/page.tsx
+++ frontend/src/app/Inicio/page.tsx
@@ -1,46 +1,17 @@
-'use client'
-import { useEffect, useState } from "react";
-import CourseCard from "@/src/components/CourseCard";
+import CourseList from "./CourseList";
 import { getCourses } from "@/src/services/courseServices";
 import Course from "@/src/types/Course";
-import Notification from "@/src/utils/notifications";
 
-export default function Inicio() {
-  const [courses, setCourses] = useState<Course[]>([]);
+export default async function Inicio() {
+  let courses: Course[] = [];
 
-  const fetchCourses = async () => {
-    try {
-        const response = await getCourses()
-        const data = response.data
-
-        if(response && data){
-            setCourses(data)
-        }
-    } catch (error) {
-        Notification({message: "Error obteniendo cursos", type:"error"})
-    }
+  try {
+    const response = await getCourses();
+    if (!response?.data) throw new Error("No se recibieron datos");
+    courses = response.data;
+  } catch (e) {
+    console.error("[Inicio] Error:", e);
   }
 
-  useEffect(() => {
-    fetchCourses();
-  }, []);
-
-  return (
-    <div>
-      <h1 className="text-center underline text-3xl font-bold mb-5">Cursos</h1>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-      {courses.map((c) => (
-        <div key={c._id} className="max-w-md"> 
-          <CourseCard 
-            _id={c._id} 
-            name={c.name} 
-            thumbnail={c.thumbnail} 
-            description={c.description}
-          />
-        </div>
-      ))}
-    </div>
-    </div>
-    
-  );
+  return <CourseList initialCourses={courses} />;
 }
