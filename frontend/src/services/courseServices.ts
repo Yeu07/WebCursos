e42d583ca7a1d004c97b6907fb09a1337b1890d4
Yeu <yeumensilva9@gmail.com>
2026-03-23 20:22:41 -0300

    Add get one course

--- frontend/src/services/courseServices.ts
+++ frontend/src/services/courseServices.ts
@@ -4,3 +4,7 @@ import Course from "../types/Course";
 export const getCourses = () => {
     return request<Course[]>(`/cursos`)
 }
+
+export const getCourseById = (id: string) => {
+  return request<Course>(`/cursos/${id}`);
+};
